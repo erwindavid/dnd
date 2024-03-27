acd6d988e973b7ae148849ae9d73cfb76de7131a
Erwin <36957018+erwindavid@users.noreply.github.com>
2024-03-26 21:20:40 -0400

    tablet updates

--- src/Table.svelte
+++ src/Table.svelte
@@ -12,21 +12,64 @@
     touched.x = e.touches[0].clientX - e.target.parentElement.offsetLeft;
     touched.y = e.touches[0].clientY - e.target.parentElement.offsetTop;
     }
+
     function drag(e) {
     position.x = e.touches[0].clientX - touched.x;
     position.y = e.touches[0].clientY - touched.y;
     }
+
+	function drag2(e){
+    position.x = e.clientX - touched.x;
+    position.y = e.clientY - touched.y;
+	}
+
     function resize(e) {
     height = e.touches[0].clientY - position.y - 50;
     }
+
+	function resize2(e) {
+    height = e.clientY - position.y - 50;
+    }
+
+	function tgm(e) {
+	touched.x = e.clientX - e.target.parentElement.offsetLeft;
+	touched.y = e.clientY - e.target.parentElement.offsetTop;
+	document.addEventListener('mousemove',resize2)
+	document.addEventListener('mouseup',()=> {
+		document.removeEventListener('mousemove',resize2)
+		})
+	}
+
+	function touch2(e) {
+	touched.x = e.clientX - e.target.parentElement.offsetLeft;
+	touched.y = e.clientY - e.target.parentElement.offsetTop;
+	document.addEventListener('mousemove',drag2)
+	document.addEventListener('mouseup',()=> {
+		document.removeEventListener('mousemove',drag2)
+		})
+	}
+
+
 </script>
 
-<div class="container" style={`top:${position.y}px; left:${position.x}px; height: ${maxheight}px;`}>
-    <div class="header" on:touchstart={touch} on:touchmove={drag}>{name}</div>
+<div 
+	class="container" 
+	style={`top:${position.y}px; left:${position.x}px; height: ${maxheight}px;`}
+>
+    <div 
+		class="header" 
+		on:touchstart={touch} 
+		on:touchmove={drag} 
+		on:mousedown={touch2}
+	>{name}</div>
     <div class="body">
         <slot/>
     </div>
-    <div class="resizer" on:touchmove={resize}></div>
+    <div 
+		class="resizer"
+		on:touchmove={resize} 
+		on:mousedown={tgm}
+	></div>
 </div>
 
 <style>
@@ -40,6 +83,9 @@
     height: 30px;
     background-color: teal;
     }
+	.header:hover {
+	cursor: grab;
+	}
     .body {
     overflow-y: scroll;
     height: 100%;
@@ -49,4 +95,5 @@
     height: 30px;
     background-color: grey;
     }
+	
 </style>
